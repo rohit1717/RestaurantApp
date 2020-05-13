6178d6501c578d9af0fe6e2fa95b40ebd9d3af0f
rohit singh <rohitsingh17172000.com>
2020-05-13 23:47:56 +0530

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import {ProcessHTTPMsgService} from './services/process-httpmsg.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { baseURL } from "./shared/baseurl";
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -54,7 +55,8 @@ import { baseURL } from "./shared/baseurl";
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
